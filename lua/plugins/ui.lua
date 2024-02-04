73e23410cfc02c1c30862cd98d6b7ffc453800e4
Adrian HM <adriandev@Adrians-MacBook-Pro.local>
2024-02-04 18:17:33 +0100

    adds copilot icon in bottom line

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -117,6 +117,24 @@ return {
 				["InProgress"] = Util.ui.fg("DiagnosticWarn"),
 			}
 			opts.options.theme = "tokyonight"
+			table.insert(opts.sections.lualine_x, 2, {
+				function()
+					local icon = require("lazyvim.config").icons.kinds.Copilot
+					local status = require("copilot.api").status.data
+					return icon .. (status.message or "")
+				end,
+				cond = function()
+					local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
+					return ok and #clients > 0
+				end,
+				color = function()
+					if not package.loaded["copilot"] then
+						return
+					end
+					local status = require("copilot.api").status.data
+					return colors[status.status] or colors[""]
+				end,
+			})
 		end,
 	},
 	-- filename
