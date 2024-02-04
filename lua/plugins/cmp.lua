06b8a60f8f3da40b8cd6325e7d10da061e765738
Adrian HM <adriandev@Adrians-MacBook-Pro.local>
2024-02-04 09:51:45 +0100

    add keymapping for mac suuport cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -46,6 +46,7 @@ return {
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-k>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				}),
