ce42638b1527bfd341df9db9f552c7f9816383eb
adrianhm13 <adrian.hervasmartinez@gmail.com>
2024-10-18 13:03:53 +0200

    adjust trouble lsp view

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -4,12 +4,12 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
 		modes = {
-			symbols = { -- Configure symbols mode
+			lsp = {
 				win = {
 					type = "split", -- split window
 					relative = "win", -- relative to current window
 					position = "right", -- right side
-					size = 1.0, -- 30% of the window
+					size = 0.3, -- 30% of the window
 				},
 			},
 		},
