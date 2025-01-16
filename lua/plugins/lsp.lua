03dd0fa0a8a7b0bd50ca07c8b3a6e77a8e785b4c
Adrian <adrian.hervasmartinez@gmail.com>
2025-01-16 14:56:06 +0100

    removes prettier action

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -34,7 +34,6 @@ return {
 				"luacheck",
 				"shellcheck",
 				"shfmt",
-				"prettierd",
 				"tailwindcss-language-server",
 				"gopls",
 				"css-lsp",
@@ -51,24 +50,8 @@ return {
 
 			local opts = {
 				sources = {
-					null_ls.builtins.formatting.prettierd,
 					null_ls.builtins.diagnostics.phpstan,
 				},
-				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({
-							group = augroup,
-							buffer = bufnr,
-						})
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.format({ bufnr = bufnr })
-							end,
-						})
-					end
-				end,
 			}
 			return opts
 		end,
