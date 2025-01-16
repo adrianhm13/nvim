4b052575ff26d2b2e662c9b40fc66c50369cbe9b
Adrian <adrian.hervasmartinez@gmail.com>
2025-01-16 14:56:23 +0100

    adds key to open git files with lazygit

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,6 +4,16 @@ return {
 		input = { enabled = false },
 		scroll = { enabled = false },
 		statuscolumn = { enabled = true },
+		picker = { enabled = true },
 		dashboard = { example = "advanced" },
+	},
+	keys = {
+		{
+			"<leader>gf",
+			function(opts)
+				Snacks.lazygit.log_file(opts)
+			end,
+			desc = "Git Logs file",
+		},
 	},
 }
